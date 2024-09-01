7705503ddae9c0316e7da8df1e8ab238aaee31f1
avicity <79248306+avicityv@users.noreply.github.com>
2024-09-01 20:44:20 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,42 @@
-ssh-keygen -t ed25519
-
-su -
-visudo    
-
-su <_USERNAME_>
-cd 
-
-sudo apt install git  
-sudo apt install vim  
-
-
-## cd в рабочую папку  
-git clone https://gitlab.com/ksxack/administrator-kubernetes-junior
+sudo apt update
+sudo apt install software-properties-common 
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
+python3.9 --version        #installing py3.9 for ansible
+sudo apt-get install python3.9-venv
+sudo apt install git       #installing git
+sudo apt install vim       #intalling vim
+# Downloading pip, make PATH for ansible
+sudo apt install python3.9-distutils
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+echo 'export PATH=$PATH:~/.local/bin' >> ~/.profile
+# Downloading ansible
+python3.9 -m pip install --user ansible-core==2.14.8 
+ansible --version
+#rm conflict packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done  
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update        #installing docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644" sh -  #installing k3s
+## Postinstall 
+export KUBECONFIG=~/.kube/config
+mkdir ~/.kube 2> /dev/null
+sudo k3s kubectl config view --raw > "$KUBECONFIG"
+chmod 600 "$KUBECONFIG"
+#Reboot system
+sudo reboot
