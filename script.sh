2cb0885110f744ac2ad5c367399a2b7264d2b6d4
avicity <79248306+avicityv@users.noreply.github.com>
2024-09-01 20:29:18 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,14 @@
+ssh-keygen -t ed25519
+
+su -
+visudo    
+
+su <_USERNAME_>
+cd 
+
+sudo apt install git  
+sudo apt install vim  
+
+
+## cd в рабочую папку  
+git clone https://gitlab.com/ksxack/administrator-kubernetes-junior
