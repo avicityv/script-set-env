3bcf2366ac9cf7e7ac08066dfcbf4de587b86da6
avicity <79248306+avicityv@users.noreply.github.com>
2024-09-01 21:44:10 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,9 +29,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update        #installing docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
+sudo groupadd docker || true & sudo usermod -aG docker $USER & newgrp docker 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644" sh -  #installing k3s
 ## Postinstall 
 export KUBECONFIG=~/.kube/config
